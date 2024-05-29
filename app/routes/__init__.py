a2ad3740411bf3babf8026382ffde664f973282a
Aubrie <aubriewoodbine@yahoo.com>
2024-05-29 11:45:29 -0500

    still working on setting up AWS

---
+++ app/routes/__init__.py
@@ -0,0 +1,3 @@
+from .auth import auth_routes
+from .user import user_routes
+from .image import image_routes
--- frontend/src/router/Layout.jsx
+++ frontend/src/router/Layout.jsx
@@ -4,6 +4,7 @@ import { useDispatch } from "react-redux";
 import { ModalProvider, Modal } from "../context/Modal";
 import { thunkAuthenticate } from "../redux/session";
 import Navigation from "../components/Navigation/Navigation";
+import TestImageForm from '../components/TestImageForm/TestImageForm'
 
 export default function Layout() {
   const dispatch = useDispatch();
@@ -17,6 +18,7 @@ export default function Layout() {
       <ModalProvider>
         <Navigation />
         {isLoaded && <Outlet />}
+          <TestImageForm />
         <Modal />
       </ModalProvider>
     </>
