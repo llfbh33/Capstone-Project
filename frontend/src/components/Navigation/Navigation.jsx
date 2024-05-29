0bda3257c977055968450dc17e2a7e4cf24fe1e8
Aubrie <aubriewoodbine@yahoo.com>
2024-05-29 18:32:30 -0500

    making sure a build runs fine for render test

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,24 +1,21 @@
-import { NavLink } from "react-router-dom";
-import ProfileButton from "./ProfileButton";
+// import { NavLink } from "react-router-dom";
+// import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
 
 import { useState, useEffect, useRef } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { thunkLogout } from "../../redux/session";
 import OpenModalMenuItem from "./OpenModalMenuItem";
 import LoginFormModal from "../LoginFormModal";
 import SignupFormModal from "../SignupFormModal";
 
 function Navigation() {
-  const dispatch = useDispatch();
   const [showMenu, setShowMenu] = useState(false);
-  const user = useSelector((store) => store.session.user);
+  // const user = useSelector((store) => store.session.user);
   const ulRef = useRef();
 
-  const toggleMenu = (e) => {
-    e.stopPropagation(); // Keep from bubbling up to document and triggering closeMenu
-    setShowMenu(!showMenu);
-  };
+  // const toggleMenu = (e) => {
+  //   e.stopPropagation(); // Keep from bubbling up to document and triggering closeMenu
+  //   setShowMenu(!showMenu);
+  // };
 
   useEffect(() => {
     if (!showMenu) return;
@@ -36,11 +33,6 @@ function Navigation() {
 
   const closeMenu = () => setShowMenu(false);
 
-  const logout = (e) => {
-    e.preventDefault();
-    dispatch(thunkLogout());
-    closeMenu();
-  };
   return (
     <div id='landingpage-navigation-container' >
         <img src='https://profile-images-pencrafted-capstone.s3.us-west-2.amazonaws.com/title-00366B.png' />
--- frontend/src/components/TestImageForm/TestImageForm.jsx
+++ frontend/src/components/TestImageForm/TestImageForm.jsx
@@ -1,58 +1,58 @@
-import { useEffect, useState } from "react";
-import { createImage } from "../../redux/image";
-import { useDispatch } from "react-redux";
+// import { useEffect, useState } from "react";
+// import { createImage } from "../../redux/image";
+// import { useDispatch } from "react-redux";
 
-function TestImageForm() {
-    const dispatch = useDispatch()
-    // const [userId, setUserId] = useState('')
-    const [image, setImage] = useState('')
-    // const [imageLoading, setImageLoading] = useState(false)
+// function TestImageForm() {
+//     const dispatch = useDispatch()
+//     // const [userId, setUserId] = useState('')
+//     const [image, setImage] = useState('')
+//     // const [imageLoading, setImageLoading] = useState(false)
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        let reader = new FileReader();
-        const formData = new FormData();
-        console.log('formData without image', formData)
-        console.log('image before it goes into formData', image)
-        if (image[0]) {
-            reader.readAsDataURL(image[0]);
-          }
-        reader.onload = (readerEvent) => {
-            console.log('saved', readerEvent.target.result)
-        formData.append("image", readerEvent.target.result);
+//     const handleSubmit = async (e) => {
+//         e.preventDefault();
+//         let reader = new FileReader();
+//         const formData = new FormData();
+//         console.log('formData without image', formData)
+//         console.log('image before it goes into formData', image)
+//         if (image[0]) {
+//             reader.readAsDataURL(image[0]);
+//           }
+//         reader.onload = (readerEvent) => {
+//             console.log('saved', readerEvent.target.result)
+//         formData.append("image", readerEvent.target.result);
 
-        // setImageLoading(true);
-        // await dispatch(createImage(formData.image));
-        // history.pushState('/images')
-        }
-        console.log('formData with image', formData)
-    }
+//         // setImageLoading(true);
+//         // await dispatch(createImage(formData.image));
+//         // history.pushState('/images')
+//         }
+//         console.log('formData with image', formData)
+//     }
 
-    useEffect(() => {
-        console.log('image', image)
-    }, [image])
+//     useEffect(() => {
+//         console.log('image', image)
+//     }, [image])
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit} encType="multipart/form-data">
-                {/* <label>User Id</label>
-                <input
-                    type='number'
-                    value={userId}
-                    onChange={(e) => setUserId(e.target.value)}></input>
-                <label>Profile image</label> */}
-                <input
-                    type='file'
-                    accept='image/*'
-                    onChange={(e) => setImage(e.target.files)}
-                ></input>
-                <button type="submit">Add Profile Image</button>
-            </form>
-            <div>
-                <img src='https://profile-images-pencrafted-capstone.s3.us-west-2.amazonaws.com/pi-default-5.png' />
-            </div>
-        </div>
-    )
-}
+//     return (
+//         <div>
+//             <form onSubmit={handleSubmit} encType="multipart/form-data">
+//                 {/* <label>User Id</label>
+//                 <input
+//                     type='number'
+//                     value={userId}
+//                     onChange={(e) => setUserId(e.target.value)}></input>
+//                 <label>Profile image</label> */}
+//                 <input
+//                     type='file'
+//                     accept='image/*'
+//                     onChange={(e) => setImage(e.target.files)}
+//                 ></input>
+//                 <button type="submit">Add Profile Image</button>
+//             </form>
+//             <div>
+//                 <img src='https://profile-images-pencrafted-capstone.s3.us-west-2.amazonaws.com/pi-default-5.png' />
+//             </div>
+//         </div>
+//     )
+// }
 
-export default TestImageForm;
+// export default TestImageForm;
