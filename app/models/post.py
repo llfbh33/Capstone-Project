da06f4be08c1253981827be36f2dffe624b4fd01
Aubrie <aubriewoodbine@yahoo.com>
2024-05-29 21:50:05 -0500

    completed models and relationships

--- app/models/post.py
+++ app/models/post.py
@@ -1,6 +1,6 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
-from datetime import date
+from datetime import datetime
 
 
 class Post(db.Model):
@@ -12,8 +12,10 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     entry_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('entries.id')), nullable=False)
     message = db.Column(db.String(250))
-    created_at = db.Column(db.Date, default=date.now())
-    updated_at = db.Column(db.Date, default=date.now())
+    created_at = db.Column(db.Date, default=datetime.now())
+    updated_at = db.Column(db.Date, default=datetime.now())
+
+    entries = db.relationship('Entry', back_populates='posts')
 
     def to_dict(self):
         return {
