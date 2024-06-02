12d683d9280d740999fbd94db3b8148a995b7858
Aubrie <aubriewoodbine@yahoo.com>
2024-06-02 12:26:41 -0500

    can remove a post from the home page

--- frontend/src/components/Modals/PostModals/RemovePostModal.jsx
+++ frontend/src/components/Modals/PostModals/RemovePostModal.jsx
@@ -10,12 +10,11 @@ function RemovePostModal({post}) {
     const deletePost = async () => {
         await dispatch(thunkDeletePost(post.post))
         await dispatch(thunkLoadEntries())
-        closeModal()
+        await closeModal()
         return
     }
 
     const doNotDelete = () => {
-        console.log(post.post)
         closeModal()
         return
     }
@@ -23,7 +22,7 @@ function RemovePostModal({post}) {
     return (
         <div id='post-remove-modal-container'>
             <h2>{`Are you sure you want to remove "${post.name}" from the public feed?`}</h2>
-            <h4 className="post-remove-disclaimer">Don't worry, all the comments on your post will stay with your entry so you can continue to have the advice of other members.</h4>
+            <h4 className="post-remove-disclaimer">{`Don't worry, all the comments on your post will stay with your entry so you can continue to have the advice of other members.`}</h4>
             <div id='remove-post-button-container'>
                 <button className="modal-button delete-comment-button" onClick={deletePost}>Yes, Remove Post</button>
                 <button className="modal-button delete-comment-button" onClick={doNotDelete}>No, Keep Post Public</button>
