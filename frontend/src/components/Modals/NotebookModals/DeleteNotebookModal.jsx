72cee384d78fdadefedb17a156b90a0d322a7465
Aubrie <aubriewoodbine@yahoo.com>
2024-06-02 12:35:15 -0500

    can delete an entry with a modal from the home page

--- frontend/src/components/Modals/NotebookModals/DeleteNotebookModal.jsx
+++ frontend/src/components/Modals/NotebookModals/DeleteNotebookModal.jsx
@@ -1,10 +1,30 @@
-
+import { useDispatch } from "react-redux"
+import { thunkDeleteNotebook } from "../../../redux/notebook"
+import { useModal } from "../../../context/Modal"
 
 function DeleteNotebookFormModal({notebook}) {
+    const dispatch = useDispatch()
+    const { closeModal } = useModal();
+
+    const deleteNotebook = async () => {
+        await dispatch(thunkDeleteNotebook(notebook.id))
+        await closeModal()
+        return
+    }
+
+    const doNotDelete = () => {
+        closeModal()
+        return
+    }
 
     return (
-        <div>
-            <h1>I am the all mighty delete modal!</h1>
+        <div id='post-remove-modal-container'>
+            <h2>{`Are you sure you want to delete "${notebook.name}" ?`}</h2>
+            <h4 className="post-remove-disclaimer">{`Deleting your notebook will also delete all of the entries within it. Are you sure?.`}</h4>
+            <div id='remove-post-button-container'>
+                <button className="modal-button delete-comment-button" onClick={deleteNotebook}>Yes, Delete Notebook</button>
+                <button className="modal-button delete-comment-button" onClick={doNotDelete}>No, Keep My Notebook</button>
+            </div>
         </div>
     )
 }
