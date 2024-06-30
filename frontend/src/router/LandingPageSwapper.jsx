425000722bff0d8628f28b3768e9c9f2586d7d95
Aubrie <aubriewoodbine@yahoo.com>
2024-06-30 14:17:50 -0500

    on refresh, set a loading screen instead of switch to landing page while checking for user

--- frontend/src/router/LandingPageSwapper.jsx
+++ frontend/src/router/LandingPageSwapper.jsx
@@ -1,17 +1,24 @@
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import Layout from "./Layout";
 import LandingPage from "../components/LandingPage/LandingPage";
 import { thunkAuthenticate } from "../redux/session";
+import LoadingPage from "../components/LoadingPage/LoadingPage";
 
 function LandingPageSwapper() {
     const user = useSelector(state => state.session.user);
+    const [loading, setLoading] = useState(true);
     const dispatch = useDispatch();
 
     useEffect(() => {
-        dispatch((thunkAuthenticate()))
+        dispatch((thunkAuthenticate())).then(() => {
+            setLoading(false)
+        })
+
     }, [dispatch]);
+
+    if (loading) return <LoadingPage />
 
     return (user) ? <Layout /> : <LandingPage />
 }
