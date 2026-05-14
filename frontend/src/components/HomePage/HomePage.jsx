f44e2038467d83871a4426eb54b8947434506cd0
llfbh33 <aubriewoodbine@yahoo.com>
2026-05-13 21:43:30 -0500

    refactoring and updating home page

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -16,7 +16,7 @@ import LoadingPage from "../LoadingPage/LoadingPage";
 
 
 
-function HomePage () {
+function HomePage() {
     const user = useSelector(state => state.session.user);
     const notebooks = useSelector(state => state.notebooks);
     const entries = useSelector(state => state.entries);
@@ -36,9 +36,9 @@ function HomePage () {
 
     const handleClickNotebook = (id) => {
         const newActiveState = {
-            main: {title: 'notebooks', route: '/notebooks', open: true},
-            mid: { title: id, route: `/notebook/${id}`, open: true},
-            small: { title: null, route: null, open: false}
+            main: { title: 'notebooks', route: '/notebooks', open: true },
+            mid: { title: id, route: `/notebook/${id}`, open: true },
+            small: { title: null, route: null, open: false }
         };
         setActiveNav(newActiveState);
         navigate(`/notebook/${id}`);
@@ -61,72 +61,19 @@ function HomePage () {
 
     return (
         <div className="main-container">
-            <h1 className='title page-title'>{`${user?.name}'s Home`}</h1>
-            {/* <p className="title page-subtitle">Recently Created Notebooks</p> */}
-            <div id='homepage-card-container'>
-
-                {Object.values(notebooks).reverse().slice(0,3).map(notebook => (
-                    <div key={notebook?.id} className="notebook-card-container-media-query">
-                        <div className="homepage-notebook-card"  >
-                            <div className="homepage-notebook-card-details" onClick={() => handleClickNotebook(notebook.id)}>
-                                <div>
-                                    <div>{notebook?.name}</div>
-                                </div>
-                                <div className='notebook-about-section-container'>
-                                    <div>{`${notebook?.about.slice(0, 70)}...`}</div>
-                                </div>
-                             </div>
-                            <div className="notebook-edit-delete-container">
-                                <div className="homepage-edit-notebook">
-                                    <OpenModalMenuItem
-                                    itemText={<FaEdit />}
-                                    modalComponent={<EditNotebookFormModal notebook={notebook} />}
-                                    />
-                                </div>
-                                <div className="homepage-edit-notebook">
-                                    <OpenModalMenuItem
-                                    itemText={<BsTrash3Fill />}
-                                    modalComponent={<DeleteNotebookFormModal notebook={notebook} />}
-                                    />
-                                </div>
-                            </div>
+            <div className="child-container">
+                <div className="child-container-two">
+                    <div className="padding-container-header">
+                        <h1 className="page-title">{`Welcome Back, ${user?.name}! 👋`}</h1>
+                        <p className="page-sub-title">A snapshot of your writing journey.</p>
+                    </div>
+                    <div className="items-container">
+                        <div className="item-container">
+                            
                         </div>
                     </div>
-                ))}
-                {/* <div id='homepage-new-notebook-card' onClick={handleNewNotebook}>
-                    <div id='create-notebook-title'>
-                        Ready to write? Create a notebook to get started!
-                    </div>
-                </div> */}
-
-            </div>
-            <p className="title page-subtitle">Recently Created Entries</p>
-                <div id='homepage-card-container'>
-                    {entries
-                        ? Object.values(entries).reverse().slice(0,3).map(entry => (
-                            <div key={entry.id}>
-                                <div className="homepage-notebook-card"  >
-                                    <div className="homepage-notebook-card-details" onClick={() => handleClickEntry(entry)}>
-                                        <div>{entry?.name}</div>
-                                    </div>
-                                    <div className="notebook-edit-delete-container">
-                                        <div className='homepage-edit-notebook'>
-                                            <OpenModalMenuItem
-                                            itemText={<BsTrash3Fill />}
-                                            modalComponent={<DeleteEntryFormModal entry={entry} />}
-                                            />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            ))
-                    : ''}
-                    {/* <div id='homepage-new-notebook-card' onClick={handleNewEntry} >
-                        <div id='create-notebook-title' >
-                            Click here to create a new Entry
-                        </div>
-                    </div> */}
                 </div>
+            </div>
         </div>
     )
 }
