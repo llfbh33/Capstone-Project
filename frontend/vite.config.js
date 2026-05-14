f1bc14e47d8758ae3d93cbf865a05745a44a1699
llfbh33 <aubriewoodbine@yahoo.com>
2026-05-13 21:00:58 -0500

    set up for production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,28 +3,7 @@ import eslintPlugin from "vite-plugin-eslint";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: [
-    react(),
-    eslintPlugin({
-      lintOnStart: true,
-      failOnError: mode === "production",
-    }),
-  ],
-  build: {
-    chunkSizeWarningLimit: 1000,
-  },
-  server: {
-    open: true,
-    proxy: {
-      "/api": "http://127.0.0.1:8000",
-    }
-  },
-}));
-
-// Exchange the functions before pushing to prod
-
-// export default defineConfig((mode) => ({
+// export default defineConfig(({ mode }) => ({
 //   plugins: [
 //     react(),
 //     eslintPlugin({
@@ -32,11 +11,32 @@ export default defineConfig(({ mode }) => ({
 //       failOnError: mode === "production",
 //     }),
 //   ],
-//   build: { chunkSizeWarningLimit: 1000, },
-//   server: mode === "development" ? {
+//   build: {
+//     chunkSizeWarningLimit: 1000,
+//   },
+//   server: {
 //     open: true,
 //     proxy: {
-//       '/api': "https://127.0.0.1:8000",  // needs to be 5000 on laptop   
-//     },
-//   } : undefined,
+//       "/api": "http://127.0.0.1:8000",
+//     }
+//   },
 // }));
+
+// Exchange the functions before pushing to prod
+
+export default defineConfig((mode) => ({
+  plugins: [
+    react(),
+    eslintPlugin({
+      lintOnStart: true,
+      failOnError: mode === "production",
+    }),
+  ],
+  build: { chunkSizeWarningLimit: 1000, },
+  server: mode === "development" ? {
+    open: true,
+    proxy: {
+      '/api': "https://127.0.0.1:8000",  // needs to be 5000 on laptop   
+    },
+  } : undefined,
+}));
