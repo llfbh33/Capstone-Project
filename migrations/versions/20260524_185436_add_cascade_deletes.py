ebb9e26a971ffe66c6330b318a0d239fd7dc9016
llfbh33 <aubriewoodbine@yahoo.com>
2026-05-24 19:47:53 -0500

    did not save migration file

--- migrations/versions/20260524_185436_add_cascade_deletes.py
+++ migrations/versions/20260524_185436_add_cascade_deletes.py
@@ -8,131 +8,61 @@ Create Date: 2026-05-24 18:54:36.717099
 from alembic import op
 import sqlalchemy as sa
 
+
+# revision identifiers, used by Alembic.
 revision = 'd1056550ed9c'
 down_revision = 'ed51221efa9a'
 branch_labels = None
 depends_on = None
 
-naming_convention = {
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s"
-}
-
 
 def upgrade():
-    with op.batch_alter_table('activities', schema=None, naming_convention=naming_convention) as batch_op:
-        batch_op.drop_constraint('fk_activities_user_id_users', type_='foreignkey')
-        batch_op.create_foreign_key(
-            'fk_activities_user_id_users',
-            'users',
-            ['user_id'],
-            ['id'],
-            ondelete='CASCADE'
-        )
+    with op.batch_alter_table('activities', schema=None) as batch_op:
+        batch_op.drop_constraint('activities_user_id_fkey', type_='foreignkey')
+        batch_op.create_foreign_key('fk_activities_user_id_users', 'users', ['user_id'], ['id'], ondelete='CASCADE')
 
-    with op.batch_alter_table('comments', schema=None, naming_convention=naming_convention) as batch_op:
-        batch_op.drop_constraint('fk_comments_user_id_users', type_='foreignkey')
-        batch_op.create_foreign_key(
-            'fk_comments_user_id_users',
-            'users',
-            ['user_id'],
-            ['id'],
-            ondelete='CASCADE'
-        )
+    with op.batch_alter_table('comments', schema=None) as batch_op:
+        batch_op.drop_constraint('comments_user_id_fkey', type_='foreignkey')
+        batch_op.create_foreign_key('fk_comments_user_id_users', 'users', ['user_id'], ['id'], ondelete='CASCADE')
 
-    with op.batch_alter_table('entries', schema=None, naming_convention=naming_convention) as batch_op:
-        batch_op.drop_constraint('fk_entries_user_id_users', type_='foreignkey')
-        batch_op.drop_constraint('fk_entries_notebook_id_notebooks', type_='foreignkey')
+    with op.batch_alter_table('entries', schema=None) as batch_op:
+        batch_op.drop_constraint('entries_user_id_fkey', type_='foreignkey')
+        batch_op.drop_constraint('entries_notebook_id_fkey', type_='foreignkey')
+        batch_op.create_foreign_key('fk_entries_user_id_users', 'users', ['user_id'], ['id'], ondelete='CASCADE')
+        batch_op.create_foreign_key('fk_entries_notebook_id_notebooks', 'notebooks', ['notebook_id'], ['id'], ondelete='CASCADE')
 
-        batch_op.create_foreign_key(
-            'fk_entries_user_id_users',
-            'users',
-            ['user_id'],
-            ['id'],
-            ondelete='CASCADE'
-        )
+    with op.batch_alter_table('notebooks', schema=None) as batch_op:
+        batch_op.drop_constraint('notebooks_user_id_fkey', type_='foreignkey')
+        batch_op.create_foreign_key('fk_notebooks_user_id_users', 'users', ['user_id'], ['id'], ondelete='CASCADE')
 
-        batch_op.create_foreign_key(
-            'fk_entries_notebook_id_notebooks',
-            'notebooks',
-            ['notebook_id'],
-            ['id'],
-            ondelete='CASCADE'
-        )
-
-    with op.batch_alter_table('notebooks', schema=None, naming_convention=naming_convention) as batch_op:
-        batch_op.drop_constraint('fk_notebooks_user_id_users', type_='foreignkey')
-        batch_op.create_foreign_key(
-            'fk_notebooks_user_id_users',
-            'users',
-            ['user_id'],
-            ['id'],
-            ondelete='CASCADE'
-        )
-
-    with op.batch_alter_table('posts', schema=None, naming_convention=naming_convention) as batch_op:
+    with op.batch_alter_table('posts', schema=None) as batch_op:
         batch_op.drop_constraint('fk_posts_user_id_users', type_='foreignkey')
-        batch_op.create_foreign_key(
-            'fk_posts_user_id_users',
-            'users',
-            ['user_id'],
-            ['id'],
-            ondelete='CASCADE'
-        )
+        batch_op.create_foreign_key('fk_posts_user_id_users', 'users', ['user_id'], ['id'], ondelete='CASCADE')
+
+    # ### end Alembic commands ###
 
 
 def downgrade():
-    with op.batch_alter_table('posts', schema=None, naming_convention=naming_convention) as batch_op:
+    with op.batch_alter_table('posts', schema=None) as batch_op:
         batch_op.drop_constraint('fk_posts_user_id_users', type_='foreignkey')
-        batch_op.create_foreign_key(
-            'fk_posts_user_id_users',
-            'users',
-            ['user_id'],
-            ['id']
-        )
+        batch_op.create_foreign_key('fk_posts_user_id_users', 'users', ['user_id'], ['id'])
 
-    with op.batch_alter_table('notebooks', schema=None, naming_convention=naming_convention) as batch_op:
+    with op.batch_alter_table('notebooks', schema=None) as batch_op:
         batch_op.drop_constraint('fk_notebooks_user_id_users', type_='foreignkey')
-        batch_op.create_foreign_key(
-            'fk_notebooks_user_id_users',
-            'users',
-            ['user_id'],
-            ['id']
-        )
+        batch_op.create_foreign_key('notebooks_user_id_fkey', 'users', ['user_id'], ['id'])
 
-    with op.batch_alter_table('entries', schema=None, naming_convention=naming_convention) as batch_op:
+    with op.batch_alter_table('entries', schema=None) as batch_op:
         batch_op.drop_constraint('fk_entries_notebook_id_notebooks', type_='foreignkey')
         batch_op.drop_constraint('fk_entries_user_id_users', type_='foreignkey')
+        batch_op.create_foreign_key('entries_notebook_id_fkey', 'notebooks', ['notebook_id'], ['id'])
+        batch_op.create_foreign_key('entries_user_id_fkey', 'users', ['user_id'], ['id'])
 
-        batch_op.create_foreign_key(
-            'fk_entries_notebook_id_notebooks',
-            'notebooks',
-            ['notebook_id'],
-            ['id']
-        )
-
-        batch_op.create_foreign_key(
-            'fk_entries_user_id_users',
-            'users',
-            ['user_id'],
-            ['id']
-        )
-
-    with op.batch_alter_table('comments', schema=None, naming_convention=naming_convention) as batch_op:
+    with op.batch_alter_table('comments', schema=None) as batch_op:
         batch_op.drop_constraint('fk_comments_user_id_users', type_='foreignkey')
-        batch_op.create_foreign_key(
-            'fk_comments_user_id_users',
-            'users',
-            ['user_id'],
-            ['id']
-        )
+        batch_op.create_foreign_key('comments_user_id_fkey', 'users', ['user_id'], ['id'])
 
-    with op.batch_alter_table('activities', schema=None, naming_convention=naming_convention) as batch_op:
+    with op.batch_alter_table('activities', schema=None) as batch_op:
         batch_op.drop_constraint('fk_activities_user_id_users', type_='foreignkey')
-        batch_op.create_foreign_key(
-            'fk_activities_user_id_users',
-            'users',
-            ['user_id'],
-            ['id']
-        )
+        batch_op.create_foreign_key('activities_user_id_fkey', 'users', ['user_id'], ['id'])
 
     # ### end Alembic commands ###
