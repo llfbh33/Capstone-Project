847e2be38f0649233f3ad867373ed732b74a07fd
llfbh33 <aubriewoodbine@yahoo.com>
2026-05-29 15:54:48 -0500

    created reusable preview entry panel

--- frontend/src/components/EntryPage/AllEntries.jsx
+++ frontend/src/components/EntryPage/AllEntries.jsx
@@ -1,25 +1,16 @@
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from "react-redux";
 import { useState, useMemo, useEffect, useRef } from 'react';
-import { BsTrash3Fill } from "react-icons/bs";
-import { MdOpenInNew } from "react-icons/md";
+
 import { FaPlus } from "react-icons/fa6";
-import { GoPencil } from "react-icons/go";
-// import { FaRegStar } from "react-icons/fa";
-// import { FaStar } from "react-icons/fa";
-import DeleteEntryFormModal from '../Modals/EntryModals/DeleteEntryModal';
-import OpenModalMenuItem from '../Modals/OpenModalButton/OpenModalMenuItem';
-import { useModal } from '../../context/Modal/Modal';
-// import './NotebookPage.css'
-import { friendlyDate } from '../../utils/utils';
-import parser from 'html-react-parser'
-import { MdKeyboardArrowRight } from "react-icons/md";
-import { MdKeyboardArrowLeft } from "react-icons/md";
-import EditEntryNameFormModal from '../Modals/EntryModals/EditEntryNameModal';
 import { FaRegFileAlt } from "react-icons/fa";
+
+import { useModal } from '../../context/Modal/Modal';
+import { friendlyDate } from '../../utils/utils';
 import CreateEntryNoNotebookModal from '../Modals/EntryModals/CreateEntryNoNotebookModal';
 import SearchBar from '../ReusableComponents/SearchComponents/SearchBar';
 import SearchClearWithTitle from '../ReusableComponents/SearchComponents/SearchClearWithTitle';
+import SelectedPreview from '../ReusableComponents/SelectedPreview/SelectedPreview';
 
 
 
@@ -57,11 +48,6 @@ const AllEntries = () => {
         );
     }, [entries, search, selectedNotebook]);
 
-    const selectedIndex = useMemo(() => {
-        if (selectedEntry) {
-            return searchEntries.indexOf(selectedEntry)
-        } else return 0;
-    }, [searchEntries, selectedEntry])
 
 
     // Scroll the selected entry in the list into view
@@ -76,10 +62,7 @@ const AllEntries = () => {
 
 
 
-    const handleClickEntry = () => {
-        navigate(`/notebook/${selectedEntry.notebook_id}/entries/${selectedEntry.id}`);
-        setSelectedEntry(null);
-    }
+
 
     const handleNewEntry = () => {
         let modalComponent = <CreateEntryNoNotebookModal notebook={selectedNotebook} />
@@ -95,16 +78,6 @@ const AllEntries = () => {
         }
     };
 
-    const handleNewSelected = (direction) => {
-        let newSelected;
-        if (direction === 'left') {
-            newSelected = selectedIndex - 1 >= 0 ? searchEntries[selectedIndex - 1] : searchEntries[searchEntries.length - 1];
-        } else {
-            newSelected = selectedIndex + 1 < searchEntries.length ? searchEntries[selectedIndex + 1] : searchEntries[0];
-        }
-
-        setSelectedEntry(newSelected);
-    };
 
     const handleFilterCondition = (id) => {
         if (selectedEntry?.notebook_id !== id) {
@@ -179,75 +152,14 @@ const AllEntries = () => {
                             </div>
                         </div>
                     </div>
-                    {/* Container invisible unless an entry has been selected, this is a preview slide */}
+
                     {selectedEntry &&
-                        <div className='selected-entry-horizontal-container'>
-                            <div className='selected-entry-container'>
-                                <div className='label-and-icons'>
-                                    <div className='label-and-icons-header'>
-                                        <span className="entry-preview-title">
-                                            {selectedEntry.name}
-
-                                            <span
-                                                className="title-edit-trigger"
-                                                onClick={(e) => e.stopPropagation()}
-                                            >
-                                                <OpenModalMenuItem
-                                                    itemText={<GoPencil className="icon-container" />}
-                                                    modalComponent={<EditEntryNameFormModal entry={selectedEntry} setSelectedEntry={setSelectedEntry} />}
-                                                />
-                                            </span>
-                                        </span>
-                                        <div className='notebook-icon-container'>
-                                            <div className='icon-container' onClick={handleClickEntry}><MdOpenInNew /></div>
-                                            <div className='icon-container'>
-                                                <OpenModalMenuItem
-                                                    itemText={<BsTrash3Fill />}
-                                                    modalComponent={<DeleteEntryFormModal entry={selectedEntry} setSelectedEntry={setSelectedEntry} />}
-                                                />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div>{notebooks[selectedEntry.notebook_id].name}</div>
-                                    <div className='create-space-between'>
-                                        <div className='alignment'>
-                                            <div>{friendlyDate(selectedEntry.updated_at)}</div>
-                                            <div>•</div>
-                                            <div>{`Comments: ${selectedEntry.comments.length}`}</div>
-                                        </div>
-                                        {selectedEntry.is_public && <p className='is-published'>Published •</p>}
-                                    </div>
-                                </div>
-                                {/* <div className="selected-entry-data"> */}
-                                <div className='list-section selected-entry-data'>
-                                    <div className='scroll-contain'>
-                                        <div className="list-scroll selected-entry-data-inner">
-                                            <div className="notebook-about-section" type="HTML">{parser(selectedEntry.content)}</div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div className='selected-entry-footer'>
-                                    <div className='selected-footer-format'>
-                                        <div className='alignment movement-click' onClick={() => handleNewSelected('left')}>
-                                            <MdKeyboardArrowLeft /> Previous
-                                        </div>
-                                        <div className='alignment movement-click' onClick={() => handleNewSelected('right')}>
-                                            Next <MdKeyboardArrowRight />
-                                        </div>
-                                    </div>
-                                    <div className='selected-footer-format'>
-                                        <div className='no-movement'>
-                                            {selectedIndex - 1 >= 0 ? searchEntries[selectedIndex - 1]?.name : searchEntries[searchEntries.length - 1]?.name}
-                                        </div>
-                                        <div style={{ textAlign: "right" }} className='no-movement'>
-                                            {selectedIndex + 1 < searchEntries.length ? searchEntries[selectedIndex + 1]?.name : searchEntries[0]?.name}
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
+                        <SelectedPreview 
+                            selected={selectedEntry} 
+                            setSelected={setSelectedEntry} 
+                            searchArray={searchEntries} 
+                            showNotebook={true}
+                        />
                     }
                 </div>
             </div>
---
+++ frontend/src/components/ReusableComponents/SelectedPreview/SelectedPreview.jsx
@@ -0,0 +1,118 @@
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from "react-redux";
+import { useMemo } from "react";
+import parser from 'html-react-parser'
+
+import { GoPencil } from "react-icons/go";
+import { MdOpenInNew } from "react-icons/md";
+import { BsTrash3Fill } from "react-icons/bs";
+import { MdKeyboardArrowRight } from "react-icons/md";
+import { MdKeyboardArrowLeft } from "react-icons/md";
+
+import OpenModalMenuItem from '../../Modals/OpenModalButton/OpenModalMenuItem';
+import EditEntryNameFormModal from '../../Modals/EntryModals/EditEntryNameModal';
+import DeleteEntryFormModal from '../../Modals/EntryModals/DeleteEntryModal';
+import { friendlyDate } from '../../../utils/utils';
+
+
+const SelectedPreview = ({ selected, setSelected, searchArray, showNotebook }) => {
+    console.log(searchArray)
+    console.log(selected)
+    const navigate = useNavigate();
+    const notebooks = useSelector(state => state.notebooks);
+    const selectedIndex = useMemo(() => {
+        if (selected) {
+            return searchArray.indexOf(selected)
+        } else return 0;
+    }, [searchArray, selected]);
+
+
+    const handleClickEntry = () => {
+        navigate(`/notebook/${selected.notebook_id}/entries/${selected.id}`);
+        setSelectedEntry(null);
+    };
+
+    const handleNewSelected = (direction) => {
+        let newSelected;
+        if (direction === 'left') {
+            newSelected = selectedIndex - 1 >= 0 ? searchArray[selectedIndex - 1] : searchArray[searchArray.length - 1];
+        } else {
+            newSelected = selectedIndex + 1 < searchArray.length ? searchArray[selectedIndex + 1] : searchArray[0];
+        }
+
+        setSelected(newSelected);
+    };
+
+
+    return (
+        <div className='selected-entry-horizontal-container'>
+            <div className='selected-entry-container'>
+                <div className='label-and-icons'>
+                    <div className='label-and-icons-header'>
+                        <span className="entry-preview-title">
+                            {selected.name}
+
+                            <span
+                                className="title-edit-trigger"
+                                onClick={(e) => e.stopPropagation()}
+                            >
+                                <OpenModalMenuItem
+                                    itemText={<GoPencil className="icon-container" />}
+                                    modalComponent={<EditEntryNameFormModal entry={selected} setSelectedEntry={setSelected} />}
+                                />
+                            </span>
+                        </span>
+                        <div className='notebook-icon-container'>
+                            <div className='icon-container' onClick={handleClickEntry}><MdOpenInNew /></div>
+                            <div className='icon-container'>
+                                <OpenModalMenuItem
+                                    itemText={<BsTrash3Fill />}
+                                    modalComponent={<DeleteEntryFormModal entry={selected} setSelectedEntry={setSelected} />}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    {showNotebook && <div>{notebooks[selected.notebook_id].name}</div>}
+                    <div className='create-space-between'>
+                        <div className='alignment'>
+                            <div>{friendlyDate(selected.updated_at)}</div>
+                            <div>•</div>
+                            <div>{`Comments: ${selected.comments.length}`}</div>
+                        </div>
+                        {selected.is_public && <p className='is-published'>Published •</p>}
+                    </div>
+                </div>
+                {/* <div className="selected-entry-data"> */}
+                <div className='list-section selected-entry-data'>
+                    <div className='scroll-contain'>
+                        <div className="list-scroll selected-entry-data-inner">
+                            <div className="notebook-about-section" type="HTML">{parser(selected.content)}</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='selected-entry-footer'>
+                    <div className='selected-footer-format'>
+                        <div className='alignment movement-click' onClick={() => handleNewSelected('left')}>
+                            <MdKeyboardArrowLeft /> Previous
+                        </div>
+                        <div className='alignment movement-click' onClick={() => handleNewSelected('right')}>
+                            Next <MdKeyboardArrowRight />
+                        </div>
+                    </div>
+                    <div className='selected-footer-format'>
+                        <div className='no-movement'>
+                            {selectedIndex - 1 >= 0 ? searchArray[selectedIndex - 1]?.name : searchArray[searchArray.length - 1]?.name}
+                        </div>
+                        <div style={{ textAlign: "right" }} className='no-movement'>
+                            {selectedIndex + 1 < searchArray.length ? searchArray[selectedIndex + 1]?.name : searchArray[0]?.name}
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default SelectedPreview;
