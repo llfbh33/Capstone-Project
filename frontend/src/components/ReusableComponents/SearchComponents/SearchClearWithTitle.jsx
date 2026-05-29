8197a79a07c8f72eae3ee3753ecbb3ee064a62ca
llfbh33 <aubriewoodbine@yahoo.com>
2026-05-29 14:55:55 -0500

    updated css styles for title and clear

--- frontend/src/components/ReusableComponents/SearchComponents/SearchClearWithTitle.jsx
+++ frontend/src/components/ReusableComponents/SearchComponents/SearchClearWithTitle.jsx
@@ -1,10 +1,11 @@
 import { RxReset } from "react-icons/rx";
+import './Search.css'
 
 const SearchClearWithTitle = ({ item, handleClear }) => {
 
     return (
-        <div className="all-entries-action">
-            <p className='sub-title remove-margin'>{`Posts (${item.length})`}</p>
+        <div className="search-clear-container">
+            <p className='search-clear-title'>{`Posts (${item.length})`}</p>
             <div className='icon-container' onClick={handleClear}><RxReset /></div>
         </div>
     )
